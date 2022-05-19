496c2b6331a354e8f8486b1273df8bbc088e3ace
Rosoner <rosoner@abv.bg>
2022-05-19 12:15:29 +0300

    Add serach by text

--- NunitWebDriverTests/SoftUniTest.cs
+++ NunitWebDriverTests/SoftUniTest.cs
@@ -15,7 +15,6 @@ namespace NunitWebDriverTests
             this.driver = new ChromeDriver();
             driver.Url = "https://softuni.bg/";
             driver.Manage().Window.Maximize();
-
         }
 
         [OneTimeTearDown]
@@ -48,8 +47,7 @@ namespace NunitWebDriverTests
 
             // Assert
             Assert.That(driver.Title, Is.EqualTo(expectedTitle));
-
-            
+ 
         }
 
         [Test]
@@ -60,11 +58,36 @@ namespace NunitWebDriverTests
             driver.FindElement(By.CssSelector(".softuni-btn-primary")).Click();
             driver.FindElement(By.CssSelector(".authentication-page")).Click();
             driver.FindElement(By.Id("username")).SendKeys("user1");
+            // clear type for user1
+            //driver.FindElement(By.Id("username")).Clear();
             driver.FindElement(By.CssSelector(".authentication-page")).Click();
             driver.FindElement(By.Id("password-input")).SendKeys("user1");
             driver.FindElement(By.CssSelector(".softuni-btn")).Click();
             Assert.That(driver.FindElement(By.CssSelector("li")).Text, Is.EqualTo("Невалидно потребителско име или парола"));
             driver.Close();
+        }
+
+
+        [Test]
+        public void Test_Search_PositiveResults()
+        { 
+            // click on search button
+            var searchField = driver.FindElement(By.CssSelector(".header-search-dropdown-link .fa-search"));
+            searchField.Click();
+
+            // Type Search vale and hit Enter
+            var searchBox = driver.FindElement(By.CssSelector(".container > form #search-input"));
+
+            searchBox.Click();
+            searchBox.SendKeys("QA");
+            searchBox.SendKeys(Keys.Enter);
+
+            // Get by Text
+            var resultField = driver.FindElement(By.CssSelector(".search-title")).Text;
+            var expectedValue = "Резултати от търсене на “QA”";
+
+            Assert.That(resultField, Is.EqualTo(expectedValue));
+
         }
     }
 }
